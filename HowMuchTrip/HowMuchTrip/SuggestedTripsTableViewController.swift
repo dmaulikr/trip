67254b494ccd60e582c1e551fff41bcf3c8211ff
Jen Hamilton <jen@jshamilton.net>
2015-12-14 09:49:53 -0500

    added shuffle to suggested trips

--- HowMuchTrip/HowMuchTrip/SuggestedTripsTableViewController.swift
+++ HowMuchTrip/HowMuchTrip/SuggestedTripsTableViewController.swift
@@ -19,33 +19,12 @@ class SuggestedTripsTableViewController: UITableViewController
     {
         super.viewDidLoad()
         title = "Suggested"
-//        
-//        let fakeTrip = Trip()
-//        
-//        fakeTrip.budgetTotal = 1000.0
-//        fakeTrip.subtotalOfProperties = 0.0
-//        fakeTrip.budgetRemaining = 540.0
-//        
-//        fakeTrip.departureLocation = ""
-//        fakeTrip.destination = "Fakesville, ND"
-//        
-//        fakeTrip.dateFrom = 0.0
-//        fakeTrip.dateTo = 0.0
-//        fakeTrip.numberOfDays = 1.0
-//        fakeTrip.numberOfNights = 1.0
-//        
-//        fakeTrip.planeTicketCost = 250.0
-//        fakeTrip.dailyLodgingCost = 100.0
-//        fakeTrip.dailyFoodCost = 50.0
-//        fakeTrip.dailyOtherCost = 0.0
-//        fakeTrip.oneTimeCost = 60.0
-//        
-//        fakeTrip.totalLodgingCosts = 100.0
-//        fakeTrip.totalFoodCosts = 50.0
-//        fakeTrip.totalOtherDailyCosts = 0.0
-//        
-//        trips.append(fakeTrip)
 
+        
+        
+    }
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(true)
         loadTrips()
         
     }
@@ -57,11 +36,12 @@ class SuggestedTripsTableViewController: UITableViewController
             {
                 let tripsJSON = try NSJSONSerialization
                     .JSONObjectWithData(data, options: NSJSONReadingOptions.AllowFragments) as! [NSDictionary]
-                print(tripsJSON)
+//                print(tripsJSON)
                 for tripDict in tripsJSON
                 {
                     let aTrip = suggestedTripFromJSON(tripDict)
                     trips.append(aTrip)
+                    trips.shuffleInPlace()
                 }
             }
             catch
@@ -109,68 +89,10 @@ class SuggestedTripsTableViewController: UITableViewController
         return trip
             
         }
-        
-        //we can put this in the trip object swift
-        /*
-        let budgetTotal             = suggestedTrip["budgetTotal"]          as? Double ?? 0.0
-        var budgetRemaining         = budgetTotal
-        
-        let destination             = suggestedTrip["destination"]          as? String ?? ""
-        let departureLocation       = suggestedTrip["departureLocation"]    as? String ?? "Orlando, FL"
-        
-        var days: Double {
-            let numberOfSecondsInDay = (86400.0)
-            let maxNumOfDays: UInt32 = 20
-            return (Double(arc4random() % maxNumOfDays) * (numberOfSecondsInDay))
-        }
-        
-        let dateFrom                = NSDate(timeInterval: days, sinceDate: NSDate())
-        let dateTo                  = NSDate(timeInterval: days, sinceDate: dateFrom)
-        print(dateTo)
-        
-        let numberOfDays            = days / 86400.0
-        let numberOfNights          = numberOfDays
-        
-        var random1to10: Double {
-            return Double(arc4random() % 10)
-        }
-    
-        let planeTicketCost         = String(random1to10 * 50.0)
-        
-        let dailyLodgingCost        = String(random1to10 * 10.0)
-        
-        let dailyFoodCost           = String(random1to10 * 5.0)
-        let dailyOtherCost          = String(random1to10 * 2.0)
-        
-        let oneTimeCost             = String(0.0)
-//        let totalLodgingCosts  
-        
-        var allProperties = [String : String]()
-        
-        allProperties = [
-            "Budget"                : String(budgetTotal),
-            "Departure Location"    : departureLocation,
-            "Destination"           : destination,
-            "Date From"             : String(0.0),
-            "Date To"               : String(0.0),
-            "Plane Ticket Cost"     : String(planeTicketCost),
-            "Daily Lodging Cost"    : String(dailyLodgingCost),
-            "Daily Food Cost"       : String(dailyFoodCost),
-            "Daily Other Cost"      : String(dailyOtherCost),
-            "One Time Cost"         : String(oneTimeCost)
-        ]
-        
-        let aTrip = Calculator(dictionary: allProperties)
-        
-        return aTrip
-        */
+
         return trip
     }
-    
-//    func makeTrip(fromDictionary: Dictionary) -> Trip?
-//    {
-//        
-//    }
+
 
     // MARK: - Table view data source
 
