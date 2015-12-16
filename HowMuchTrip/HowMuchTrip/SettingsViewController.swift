738640ca7c1eb45ba0b71df4bf28d189c663e363
Chris Stomp <christopherstomp@gmail.com>
2015-12-16 10:16:36 -0500

    Added code in ViewDidLoad for FB username, email, and image capture

--- HowMuchTrip/HowMuchTrip/SettingsViewController.swift
+++ HowMuchTrip/HowMuchTrip/SettingsViewController.swift
@@ -11,17 +11,94 @@ import Parse
 
 class SettingsViewController: UIViewController
 {
-//    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var userNameLabel: UILabel!
 
     override func viewDidLoad()
     {
         super.viewDidLoad()
 
         title = "Settings"
-//        if let pUserName = PFUser.currentUser()?["username"] as? String
-//        {
-//            self.userNameLabel.text = "@" + pUserName
-//        }
+        if let pUserName = PFUser.currentUser()?["username"] as? String
+        {
+            self.userNameLabel.text = "@" + pUserName
+        }
+        
+        var requestParameters = ["fields": "id, email, first_name, last_name"]
+        
+        let userDetails = FBSDKGraphRequest(graphPath: "me", parameters: requestParameters)
+        
+        userDetails.startWithCompletionHandler { (connection, result, error:NSError!) -> Void in
+            
+            if(error != nil)
+            {
+                print("\(error.localizedDescription)")
+                return
+            }
+            
+            if(result != nil)
+            {
+                
+                let userId:String = result["id"] as! String
+                let userFirstName:String? = result["first_name"] as? String
+                let userLastName:String? = result["last_name"] as? String
+                let userEmail:String? = result["email"] as? String
+                
+                
+                print("\(userEmail)")
+                
+                let myUser:PFUser = PFUser.currentUser()!
+                
+                // Save first name
+                if(userFirstName != nil)
+                {
+                    myUser.setObject(userFirstName!, forKey: "first_name")
+                    
+                }
+                
+                //Save last name
+                if(userLastName != nil)
+                {
+                    myUser.setObject(userLastName!, forKey: "last_name")
+                }
+                
+                // Save email address
+                if(userEmail != nil)
+                {
+                    myUser.setObject(userEmail!, forKey: "email")
+                }
+                
+                dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)) {
+                    
+                    // Get Facebook profile picture
+                    let userProfile = "https://graph.facebook.com/" + userId + "/picture?type=large"
+                    
+                    let profilePictureUrl = NSURL(string: userProfile)
+                    
+                    let profilePictureData = NSData(contentsOfURL: profilePictureUrl!)
+                    
+                    if(profilePictureData != nil)
+                    {
+                        let profileFileObject = PFFile(data:profilePictureData!)
+                        myUser.setObject(profileFileObject!, forKey: "profile_picture")
+                    }
+                    
+                    
+                    myUser.saveInBackgroundWithBlock({ (success:Bool, error:NSError?) -> Void in
+                        
+                        if(success)
+                        {
+                            print("User details are now updated")
+                        }
+                        
+                    })
+                    
+                }
+                
+            }
+            
+        }
+        
+
 
     }
 
