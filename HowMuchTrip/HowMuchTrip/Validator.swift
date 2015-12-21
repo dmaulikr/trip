108487ed58a2818bae364ac163f962f533ec976e
Jen Hamilton <jen@jshamilton.net>
2015-12-21 12:31:40 -0500

    added documentation to Validator

--- HowMuchTrip/HowMuchTrip/Validator.swift
+++ HowMuchTrip/HowMuchTrip/Validator.swift
@@ -8,25 +8,48 @@
 
 import Foundation
 
+/// Class used to validate and format various text field entries.
 class Validator
 {
+    /**
+     Function that validates various entries into text fields.
+     
+     - Parameters:
+        - regEx: Format to be used for validation
+        - string: String to be validated
+
+     Formats available for validation:
+        - phone
+        - email
+        - name
+        - birthday
+     
+     - Returns: True if 'string' passes the validation, false if the 'string' fails.
+    */
     func validate(var regEx: String, string: String) -> Bool
     {
         switch regEx
         {
-        case "phone": regEx = "^\\(\\d{3}\\) \\d{3}-\\d{4}$"
-        case "email": regEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}"
-        case "name": regEx = "^[A-Za-z]{1,10}$"
-        case "birthday": regEx = "^(0[1-9]|1[012])[/](0[1-9]|[12][0-9]|3[01])[/](19|20)\\d\\d$"
-        default: print("unknown regEx: " + regEx)
+            // Allow only a number in '(xxx) xxx-xxxx format'
+            case "phone": regEx = "^\\(\\d{3}\\) \\d{3}-\\d{4}$"
+            // Allow only numbers, letters and some characters, followed by and '@', the letters and numbers, then a '.', then 2-6 letters.
+            case "email": regEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}"
+            // Allow only 1-20 letters
+            case "name": regEx = "^[A-Za-z]{1,20}$"
+            // Allow only numbers in xx/xx/(19|20)xx format
+            case "birthday": regEx = "^(0[1-9]|1[012])[/](0[1-9]|[12][0-9]|3[01])[/](19|20)\\d\\d$"
+            default: print("unknown regEx: " + regEx)
         }
         
         let test = NSPredicate(format: "SELF MATCHES %@", regEx)
+        // Test string against regEx requirements
         let result =  test.evaluateWithObject(string)
         return result
     }
     
     /*
+    
+    /// Function to format a phone number as the user enters the numbers
     func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool
     {
         //http://stackoverflow.com/questions/27609104/xcode-swift-formatting-text-as-phone-number
